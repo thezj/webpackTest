b1d556c38e5f743faf1cf1098112c1d636641817
zengjie <thezj@vip.qq.com>
2017-06-12 10:58:56 +0800

    搭建基础

--- app/index.vue
+++ app/index.vue
@@ -1,26 +1,16 @@
 <template>
     <div>
-        <p>{{greeting}} vue</p>
-        <video controls="">
-            <source :src="mp4">
-        </video>
+      index
     </div>
 </template>
 
 <script>
-import mp4 from './video.mp4'
 export default {
     data() {
         return {
-            greeting: 'hello',
-            mp4
         }
     }
 }
 </script>
-<style lang='less'>
-p {
-    border: 2px dashed #fd1
-}
-</style>
+
 
